939cce1a8df85fec9cf9d0d8518b53c85265b8c3
adrian-0316 <112898743+adrian-0316@users.noreply.github.com>
2023-05-10 23:25:37 +0300

    Add files via upload

---
+++ pp-3-1-4/src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package ru.kata.spring.boot_security.demo.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserService userService;
+
+    public WebSecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(getPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/", "/index").permitAll()
+                //.anyRequest().hasAnyRole("USER", "ADMIN")
+                .and()
+                .formLogin()
+                .successHandler(new CustomAuthenticationSuccessHandler())
+                .and()
+                .logout()
+        ;
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ pp-3-1-4/src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -0,0 +1,87 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userDao, @Lazy PasswordEncoder passwordEncoder) {
+        this.userRepository = userDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public User getUserByEmail(String email) {
+        return userRepository.getUserByEmail(email);
+    }
+    @Override
+    @Transactional(readOnly = true)
+    public User findByEmailWithRoles(String email) {
+        return userRepository.findByEmailWithRoles(email);
+    }
+    @Override
+    @Transactional
+    public void saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+    @Override
+    @Transactional
+    public void adminRedactor(User user, Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            User editUser = optionalUser.get();
+            editUser.setId(user.getId());
+            editUser.setFirstName(user.getFirstName());
+            editUser.setLastName(user.getLastName());
+            editUser.setEmail(user.getEmail());
+            editUser.setRoles(user.getRoles());
+            if (!editUser.getPassword().equals(user.getPassword())) {
+                editUser.setPassword(passwordEncoder.encode(user.getPassword()));
+            } else {
+                editUser.setPassword(user.getPassword());
+            }
+            userRepository.save(editUser);
+        }
+    }
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        userRepository.delete(userRepository.getById(id));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = findByEmailWithRoles(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user;
+    }
+
+}
